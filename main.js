c0b07ae27f9d3ee0ce4121fb6d7de9343a08d1d4
hrvstr <r.muhler@me.com>
2022-11-14 16:36:36 +0100

    Allow user to define a new grid

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 const container = document.getElementById("container");
+const changeGridButton = document.getElementById("change-grid");
 
-// Create 16x16 DIV elements inside #container
-for (i = 1; i <= 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.textContent = i;
-  container.appendChild(div);
+function createGrid(size) {
+  for (i = 1; i <= size * size; i++) {
+    const div = document.createElement("div");
+    div.textContent = i;
+    container.appendChild(div);
+    container.style.grid = `auto-flow / repeat(${size}, 1fr)`;
+    div.addEventListener("mouseenter", () => {
+      div.classList.add("active");
+    });
+  }
 }
+
+function removeGrid() {
+  const squares = container.querySelectorAll("div");
+  squares.forEach((square) => {
+    container.removeChild(square);
+  });
+}
+
+changeGridButton.addEventListener("click", () => {
+  // Get & check user input
+  let userInput = prompt("How many square per side?", 16);
+  if (isNaN(userInput)) {
+    alert("Please supply a number from 1 to 100.");
+  } else if (userInput > 100) {
+    alert("Number should not be larger than 100");
+  } else if (userInput < 1) {
+    alert("Number should be larger than 1.");
+  } else {
+    // Remove & Create grid
+    removeGrid();
+    createGrid(userInput);
+  }
+});
+
+// Create initial grid
+createGrid(16);
