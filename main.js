cff4ee32e693b0178df327eb30718c7fa6af1931
hrvstr <r.muhler@me.com>
2022-11-17 18:13:18 +0100

    Add a UI to change the grid size

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+// Nodes
 const container = document.getElementById("container");
-const changeGridButton = document.getElementById("change-grid");
+const clearGridButton = document.getElementById("clear");
+
+// Change grid
+const gridButton = document.getElementById("grid");
+const changeGridOverlay = document.getElementById("gridOverlay");
+const changeGridValue = document.getElementById("gridValue");
+const changeGridInput = document.getElementById("gridInput");
+const changeGridButton = document.getElementById("gridApply");
+
+// CSS Vars
+const bodyStyle = getComputedStyle(document.body);
+const backgroundLight = bodyStyle.getPropertyValue("--background-light");
+
+// Defaults
+const defaultGridSize = 16;
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -16,37 +31,52 @@ function createGrid(size) {
   for (i = 1; i <= size * size; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
-    container.style.grid = `auto-flow / repeat(${size}, 1fr)`;
     let brightness = 50;
     div.addEventListener("mouseenter", () => {
       brightness += 10;
       div.style.background = getRandomColor(brightness);
     });
   }
+  container.style.grid = `auto-flow / repeat(${size}, 1fr)`;
 }
 
 function removeGrid() {
-  const squares = container.querySelectorAll("div");
-  squares.forEach((square) => {
-    container.removeChild(square);
+  const pixels = container.querySelectorAll("div");
+  pixels.forEach((pixel) => {
+    container.removeChild(pixel);
   });
 }
 
-changeGridButton.addEventListener("click", () => {
-  // Get & check user input
-  let userInput = prompt("How many squares per side?", 16);
-  if (isNaN(userInput) || !userInput) {
-    alert("Please supply a number from 1 to 100.");
-  } else if (userInput > 100) {
-    alert("Number should not be larger than 100.");
-  } else if (userInput < 1) {
-    alert("Number should be larger than 1.");
+function calculateGrid(value) {
+  return `${value}px * ${value}px = ${value * value}px`;
+}
+
+function toggleChangeGridOverlay() {
+  if (changeGridOverlay.style.display == "none") {
+    changeGridOverlay.style.display = "flex";
   } else {
-    // Remove & Create grid
-    removeGrid();
-    createGrid(userInput);
+    changeGridOverlay.style.display = "none";
   }
+}
+
+// Clear artboard
+clearGridButton.addEventListener("click", () => {
+  const pixels = container.querySelectorAll("div");
+  pixels.forEach((pixel) => {
+    pixel.style.background = backgroundLight;
+  });
 });
 
+// Change grid size
+changeGridButton.addEventListener("click", () => {
+  changeGridOverlay.style.display = "none";
+  removeGrid();
+  createGrid(gridInput.value);
+});
+
+// Set initial values in
+changeGridInput.value = defaultGridSize;
+changeGridValue.value = calculateGrid(defaultGridSize);
+
 // Create initial grid
-createGrid(16);
+createGrid(defaultGridSize);
