5c68635e29a960fd2c50583ca7fe71245a670016
hrvstr <r.muhler@me.com>
2022-11-18 11:37:46 +0100

    Add a colorpicker and checkbox for a random color

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const container = document.getElementById("container");
 const clearGridButton = document.getElementById("clear");
 const instructions = document.getElementById("instructions");
 const drawModeStatusDisplay = document.getElementById("status");
+const colorPicker = document.getElementById("colorPicker");
+const randomColorCheckbox = document.getElementById("randomColor");
 
 // Change grid
 const gridButton = document.getElementById("grid");
@@ -13,13 +15,16 @@ const changeGridButton = document.getElementById("gridApply");
 
 // CSS Vars
 const bodyStyle = getComputedStyle(document.body);
-const backgroundLight = bodyStyle.getPropertyValue("--background-light");
+const tertiary = bodyStyle.getPropertyValue("--tertiary");
 const green = bodyStyle.getPropertyValue("--green");
 const red = bodyStyle.getPropertyValue("--red");
 
 // Defaults
 const defaultGridSize = 16;
 let drawModeIsActive = false;
+let currentColor = green;
+
+// Functions
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -38,11 +43,14 @@ function createGrid(size) {
     const div = document.createElement("div");
     container.appendChild(div);
     let brightness = 50;
-
     div.addEventListener("mouseenter", () => {
       if (drawModeIsActive) {
-        brightness += 10;
-        div.style.background = getRandomColor(brightness);
+        if (randomColorCheckbox.checked) {
+          brightness += 10;
+          div.style.background = getRandomColor(brightness);
+        } else {
+          div.style.background = currentColor;
+        }
       }
     });
   }
@@ -76,18 +84,25 @@ function changeDrawModeInfo() {
   }
 }
 
+// Event listeners
+
+randomColorCheckbox.addEventListener("change", () => {
+  if (randomColorCheckbox.checked) {
+    colorPicker.classList.add("hidden");
+  } else {
+    colorPicker.classList.remove("hidden");
+  }
+});
+
 // Toggle draw mode when clicking on the container
 container.addEventListener("click", () => {
   drawModeIsActive = !drawModeIsActive;
   changeDrawModeInfo();
 });
 
-// Clear artboard
-clearGridButton.addEventListener("click", () => {
-  const pixels = container.querySelectorAll("div");
-  pixels.forEach((pixel) => {
-    pixel.style.background = backgroundLight;
-  });
+// Change color
+colorPicker.addEventListener("input", () => {
+  currentColor = colorPicker.value;
 });
 
 // Change grid size
@@ -97,12 +112,19 @@ changeGridButton.addEventListener("click", () => {
   createGrid(gridInput.value);
 });
 
+// Clear artboard
+clearGridButton.addEventListener("click", () => {
+  const pixels = container.querySelectorAll("div");
+  pixels.forEach((pixel) => {
+    pixel.style.background = tertiary;
+  });
+});
+
 // Set initial values
 changeGridInput.value = defaultGridSize;
 changeGridValue.value = calculateGrid(defaultGridSize);
-// instructions.textContent =
-//   "Click on the square and move the cursor to start drawing.";
 changeDrawModeInfo();
+colorPicker.value = currentColor;
 
 // Create initial grid
 createGrid(defaultGridSize);
