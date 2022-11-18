d6daef67a283d697f6af09a1ce79e94dbbfad638
hrvstr <r.muhler@me.com>
2022-11-18 20:24:39 +0100

    Allow switching between color modes

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 // Nodes
 const container = document.getElementById("container");
 const clearGridButton = document.getElementById("clear");
+const helpButton = document.getElementById("help");
+const drawModeInfo = document.getElementById("info");
 const instructions = document.getElementById("instructions");
 const drawModeStatusDisplay = document.getElementById("status");
+const drawModeIndicator = document.getElementById("drawModeIndicator");
+const drawModeSeparator = document.getElementById("drawModeSeparator");
+const drawModeStatusCloseButton = document.getElementById("infoClose");
 const colorPicker = document.getElementById("colorPicker");
 const randomColorCheckbox = document.getElementById("randomColor");
+const randomColorLabel = document.getElementById("randomColorLabel");
+const colorModeButton = document.getElementById("colorModeButton");
 
 // Change grid
 const gridButton = document.getElementById("grid");
@@ -15,14 +22,26 @@ const changeGridButton = document.getElementById("gridApply");
 
 // CSS Vars
 const bodyStyle = getComputedStyle(document.body);
+const secondary = bodyStyle.getPropertyValue("--secondary");
 const tertiary = bodyStyle.getPropertyValue("--tertiary");
 const green = bodyStyle.getPropertyValue("--green");
 const red = bodyStyle.getPropertyValue("--red");
 
 // Defaults
 const defaultGridSize = 16;
-let drawModeIsActive = false;
+createGrid(defaultGridSize);
+
+// Draw mode
+let drawModeIsActive = true;
+changeDrawModeInfo();
+
+// Change grid
+changeGridValue.value = calculateGrid(defaultGridSize);
+
+// Color
 let currentColor = green;
+colorPicker.value = currentColor;
+randomColorCheckbox.checked = true;
 
 // Functions
 
@@ -49,7 +68,11 @@ function createGrid(size) {
           brightness += 10;
           div.style.background = getRandomColor(brightness);
         } else {
-          div.style.background = currentColor;
+          if (brightness < 100) {
+            brightness += 10;
+          } else {
+          }
+          div.style.background = currentColor + brightness;
         }
       }
     });
@@ -75,7 +98,8 @@ function changeDrawModeInfo() {
   if (drawModeIsActive) {
     drawModeStatusDisplay.style.color = green;
     drawModeStatusDisplay.textContent = "Drawing enabled.";
-    instructions.textContent = "Click the square to stop drawing.";
+    instructions.textContent =
+      "Move the cursor to draw. Click the square to stop drawing.";
   } else {
     drawModeStatusDisplay.style.color = red;
     drawModeStatusDisplay.textContent = "Drawing disabled.";
@@ -84,21 +108,33 @@ function changeDrawModeInfo() {
   }
 }
 
+function toggleDrawModeInfo() {
+  drawModeInfo.classList.toggle("hidden");
+  helpButton.classList.toggle("hidden");
+  drawModeIndicator.classList.toggle("hidden");
+  drawModeSeparator.classList.toggle("hidden");
+}
+
 // Event listeners
 
-randomColorCheckbox.addEventListener("change", () => {
-  if (randomColorCheckbox.checked) {
-    colorPicker.classList.add("hidden");
-  } else {
-    colorPicker.classList.remove("hidden");
-  }
+// Toggle color mode
+colorModeButton.addEventListener("click", () => {
+  colorPicker.classList.toggle("hidden");
+  randomColorLabel.classList.toggle("hidden");
+  randomColorCheckbox.checked = !randomColorCheckbox.checked;
 });
 
 // Toggle draw mode when clicking on the container
 container.addEventListener("click", () => {
   drawModeIsActive = !drawModeIsActive;
+  drawModeIndicator.classList.toggle("background-red");
   changeDrawModeInfo();
 });
+
+// Toggle draw mode info
+drawModeStatusCloseButton.addEventListener("click", toggleDrawModeInfo);
+helpButton.addEventListener("click", toggleDrawModeInfo);
+drawModeIndicator.addEventListener("click", toggleDrawModeInfo);
 
 // Change color
 colorPicker.addEventListener("input", () => {
@@ -119,12 +155,3 @@ clearGridButton.addEventListener("click", () => {
     pixel.style.background = tertiary;
   });
 });
-
-// Set initial values
-changeGridInput.value = defaultGridSize;
-changeGridValue.value = calculateGrid(defaultGridSize);
-changeDrawModeInfo();
-colorPicker.value = currentColor;
-
-// Create initial grid
-createGrid(defaultGridSize);
